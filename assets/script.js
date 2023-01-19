0872a1e1c3b78078323944d09a54e08c68e5a501
Moonbeam <mfzernick@gmail.com>
2023-01-18 20:22:17 -0600

    Appends to page

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,3 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 var currentHour = dayjs().hour();
 //console.log(currentHour);
 
@@ -15,6 +12,15 @@ $(function () {
     localStorage.setItem(key,value);
     // event.currentTarget.className = localStorage.getItem(value);
     // console.log(event.currentTarget);
+    $("#hour-9").children("textarea").val(localStorage.getItem('hour-9'));
+$("#hour-10").children("textarea").val(localStorage.getItem('hour-10'));
+$("#hour-11").children("textarea").val(localStorage.getItem('hour-11'));
+$("#hour-12").children("textarea").val(localStorage.getItem('hour-12'));
+$("#hour-13").children("textarea").val(localStorage.getItem('hour-13'));
+$("#hour-14").children("textarea").val(localStorage.getItem('hour-14'));
+$("#hour-15").children("textarea").val(localStorage.getItem('hour-15'));
+$("#hour-16").children("textarea").val(localStorage.getItem('hour-16'));
+$("#hour-17").children("textarea").val(localStorage.getItem('hour-17'));
   });
 
 $(".time-block").each(function() {
@@ -31,19 +37,6 @@ $(".time-block").each(function() {
         this.classList.add("past");
       }
       // console.log(this);
-})
+});
 
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //if (hours = )
-      //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-
-    //specificID = localStorage.getItem();
-
-  });
+});
